55ad5be460ca0a7275b4d81501bea6191d182eb7
heliang <961403370@qq.com>
2021-01-30 22:03:31 +0800

    +sql sfdg

---
+++ oa.sql
@@ -0,0 +1,173 @@
+-- MySQL dump 10.13  Distrib 5.6.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: oa
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `claim_voucher`
+--
+
+DROP TABLE IF EXISTS `claim_voucher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `claim_voucher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cause` varchar(100) DEFAULT NULL,
+  `create_sn` char(5) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `next_deal_sn` char(5) DEFAULT NULL,
+  `total_amount` double DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Reference_2` (`next_deal_sn`),
+  KEY `FK_Reference_3` (`create_sn`),
+  CONSTRAINT `FK_Reference_2` FOREIGN KEY (`next_deal_sn`) REFERENCES `employee` (`sn`),
+  CONSTRAINT `FK_Reference_3` FOREIGN KEY (`create_sn`) REFERENCES `employee` (`sn`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `claim_voucher`
+--
+
+LOCK TABLES `claim_voucher` WRITE;
+/*!40000 ALTER TABLE `claim_voucher` DISABLE KEYS */;
+/*!40000 ALTER TABLE `claim_voucher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `claim_voucher_item`
+--
+
+DROP TABLE IF EXISTS `claim_voucher_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `claim_voucher_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `claim_voucher_id` int(11) DEFAULT NULL,
+  `item` varchar(20) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `comment` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Reference_4` (`claim_voucher_id`),
+  CONSTRAINT `FK_Reference_4` FOREIGN KEY (`claim_voucher_id`) REFERENCES `claim_voucher` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `claim_voucher_item`
+--
+
+LOCK TABLES `claim_voucher_item` WRITE;
+/*!40000 ALTER TABLE `claim_voucher_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `claim_voucher_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deal_record`
+--
+
+DROP TABLE IF EXISTS `deal_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `deal_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `claim_voucher_id` int(11) DEFAULT NULL,
+  `deal_sn` char(5) DEFAULT NULL,
+  `deal_time` datetime DEFAULT NULL,
+  `deal_way` varchar(20) DEFAULT NULL,
+  `deal_result` varchar(20) DEFAULT NULL,
+  `comment` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Reference_5` (`claim_voucher_id`),
+  KEY `FK_Reference_6` (`deal_sn`),
+  CONSTRAINT `FK_Reference_5` FOREIGN KEY (`claim_voucher_id`) REFERENCES `claim_voucher` (`id`),
+  CONSTRAINT `FK_Reference_6` FOREIGN KEY (`deal_sn`) REFERENCES `employee` (`sn`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deal_record`
+--
+
+LOCK TABLES `deal_record` WRITE;
+/*!40000 ALTER TABLE `deal_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deal_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `sn` char(5) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`sn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('10001','总经理办公室','杭州大厦E座1201'),('10002','财务部','杭州大厦E座1202'),('10003','事业部','杭州大厦E座1101');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `sn` char(5) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `department_sn` char(5) DEFAULT NULL,
+  `post` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`sn`),
+  KEY `FK_Reference_1` (`department_sn`),
+  CONSTRAINT `FK_Reference_1` FOREIGN KEY (`department_sn`) REFERENCES `department` (`sn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES ('10001','123456','有梦想的老板','10001','总经理'),('10002','123456','苏小美','10002','财务'),('10003','123456','慕容白','10003','部门经理'),('10004','123456','王大锤','10003','员工');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-30 21:15:20
